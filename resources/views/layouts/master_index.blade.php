b36bec3cd893e8d1cb5ab9211e2eca5b5a069f26
Mr.K <48730446+MrK-07@users.noreply.github.com>
2020-08-20 11:47:08 -0300

    design index
    Ajustes no design do layout da página index

--- resources/views/layouts/master_index.blade.php
+++ resources/views/layouts/master_index.blade.php
@@ -2,7 +2,7 @@
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
@@ -11,12 +11,7 @@
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
-    <script>
-    $("#menu-toggle").click(function(e) {
-        e.preventDefault();
-        $("#wrapper").toggleClass("toggled");
-    });
-    </script>
+   
     
     
     <!-- Fonts -->
@@ -26,16 +21,15 @@
     <!-- Styles -->
     <link rel="shortcut icon" href="/img/favicon.ico" />
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <link href="css/sidebar.css" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark shadow-sm">
+        <nav class="navbar navbar-expand-md navbar-dark shadow-sm" style="background-color: #FFD700">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    <img src="/img/logo.png" style="width: 50px; margin-right: 5%;"><strong style="font-size: 30px">{{ config('app.name', 'Laravel') }}</strong>
+                    <img src="/img/logo.png" style="width: 50px; margin-right: 5%;"><strong style="font-size: 30px; color: black">{{ config('app.name', 'Laravel') }}</strong>
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -83,43 +77,11 @@
             </div>
         </nav>
     
-       
+<!-- SIDEBAR -->
 
-<!-- Sidebar -->
-<div id="wrapper">
-        <div id="sidebar-wrapper">
-            <ul class="sidebar-nav">
-                <li class="sidebar-brand">
-                    <a href="#">
-                        Start Bootstrap
-                    </a>
-                </li>
-                <li>
-                    <a href="#">Dashboard</a>
-                </li>
-                <li>
-                    <a href="#">Shortcuts</a>
-                </li>
-                <li>
-                    <a href="#">Overview</a>
-                </li>
-                <li>
-                    <a href="#">Events</a>
-                </li>
-                <li>
-                    <a href="#">About</a>
-                </li>
-                <li>
-                    <a href="#">Services</a>
-                </li>
-                <li>
-                    <a href="#">Contact</a>
-                </li>
-            </ul>
-        </div>
-    
 
-        <div style="margin-left:10%">
+<!-- FIM SIDEBAR -->
+
         <!-- PÁGINA -->
         <main class="py-4">
         
@@ -130,8 +92,7 @@
     </div>
     
 
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
-<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js" integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI" crossorigin="anonymous"></script>
     
 </body>
--- resources/views/paginas/index.blade.php
+++ resources/views/paginas/index.blade.php
@@ -2,15 +2,47 @@
 @section('pagina')
 <div class="container">
   <div class="row row-cols-3"> <!-- Itens na tela 3 colunas -->
-    <div class="col" style="text-align:center"> <!--Item -->
-        <img src="img/cao.jpg" alt="imagem" width="200" height=200/><br>
+    <div class="col" style="text-align:center; border: solid; border-color: blue; border-radius: 10%;"> <!--Item -->
+        <img class="img-fluid" src="img/cao.jpg" alt="imagem" width="150" height=150/><br>
         <label style="font-weight: bold;">Filhote de cachorro</label><br>
         <label style="font-weight: bold;">Idade:</label> 1 ano(s)<br>
         <label style="font-weight: bold;">Valor:</label>R$ 0.00<br>
-        <label style="font-weight: ;">Doação</label><br>
+        <label style="font-weight: bold; color: MediumTurquoise">Doação</label><br>
+        <a href="#"><button class="btn btn-warning">Mais Detalhes</button></a>
     </div> 
     <!-- Fim do Item -->
-    
+    <div class="col" style="text-align:center; border: solid; border-color: blue; border-radius: 10%;"> <!--Item -->
+        <img class="img-fluid" src="img/cao.jpg" alt="imagem" width="150" height=150/><br>
+        <label style="font-weight: bold;">Filhote de cachorro</label><br>
+        <label style="font-weight: bold;">Idade:</label> 1 ano(s)<br>
+        <label style="font-weight: bold;">Valor:</label>R$ 0.00<br>
+        <label style="font-weight: bold; color: MediumTurquoise">Doação</label><br>
+        <a href="#"><button class="btn btn-warning">Mais Detalhes</button></a>
+    </div> 
+    <div class="col" style="text-align:center; border: solid; border-color: blue; border-radius: 10%;"> <!--Item -->
+        <img class="img-fluid" src="img/cao.jpg" alt="imagem" width="150" height=150/><br>
+        <label style="font-weight: bold;">Filhote de cachorro</label><br>
+        <label style="font-weight: bold;">Idade:</label> 1 ano(s)<br>
+        <label style="font-weight: bold;">Valor:</label>R$ 0.00<br>
+        <label style="font-weight: bold; color: MediumTurquoise">Doação</label><br>
+        <a href="#"><button class="btn btn-warning">Mais Detalhes</button></a>
+    </div> 
+    <div class="col" style="text-align:center; border: solid; border-color: blue; border-radius: 10%;"> <!--Item -->
+        <img class="img-fluid" src="img/cao.jpg" alt="imagem" width="150" height=150/><br>
+        <label style="font-weight: bold;">Filhote de cachorro</label><br>
+        <label style="font-weight: bold;">Idade:</label> 1 ano(s)<br>
+        <label style="font-weight: bold;">Valor:</label>R$ 0.00<br>
+        <label style="font-weight: bold; color: MediumTurquoise">Doação</label><br>
+        <a href="#"><button class="btn btn-warning">Mais Detalhes</button></a>
+    </div> 
+    <div class="col" style="text-align:center; border: solid; border-color: blue; border-radius: 10%;"> <!--Item -->
+        <img class="img-fluid" src="img/cao.jpg" alt="imagem" width="150" height=150/><br>
+        <label style="font-weight: bold;">Filhote de cachorro</label><br>
+        <label style="font-weight: bold;">Idade:</label> 1 ano(s)<br>
+        <label style="font-weight: bold;">Valor:</label>R$ 0.00<br>
+        <label style="font-weight: bold; color: MediumTurquoise">Doação</label><br>
+        <a href="#"><button class="btn btn-warning">Mais Detalhes</button></a>
+    </div> 
 
     
   </div> <!-- Fim dos Itens na tela -->
